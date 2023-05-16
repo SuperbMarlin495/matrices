6e56e3d09d34944afbad951dd0c868ac11c00f2f
SuperbMarlin495 <bgsh344@gmail.com>
2023-05-15 23:36:04 -0600

    first commit

---
+++ matrices.py
@@ -0,0 +1,35 @@
+a = [[1,2,3],
+     [4,5,6]]
+
+b = [[1,2],
+     [3,4],
+     [5,6]]
+
+def multiplicacion_Matriz(a, b):
+
+    if len (a[0]) == len (b):
+
+        c = []
+        for i in range(len(a)):
+            c.append([])
+            for j in range(len(b[0])):
+                c[i].append(0)
+
+        for i in range (len(a)):
+            for j in range(len(b[0])):
+                for k in range(len(a[0])):
+                    c[i][j] += a[i][k] * b[k][j]
+        return c
+    else: 
+        return None
+
+matriz = multiplicacion_Matriz(a, b)
+
+if matriz == None:
+    print("No se puede multiplicar matriz")
+else:
+    for fila in matriz:
+        print("[", end=" ") 
+        for element in fila:
+            print(element, end=" ")
+        print("]")
